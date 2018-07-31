4983ccfde61c4cc0cdd02a3bf4d46f90ed546e78
JoobinYoun <jubin3424@gmail.com>
2018-07-31 11:58:33 +0900

    modify no-ssr wrapping on vue-parallaxy

--- pages/TokenSale.vue
+++ pages/TokenSale.vue
@@ -3,9 +3,11 @@
       <div style="color: #494949; font-size: 5rem;
                   font-weight: bolder; text-align: center; margin-top: 10vh;">TokenSale</div>
       <div style="position: relative">
+        <no-ssr>
         <parallax :fixed="true">
           <img src="../static/img/image2.jpg">
         </parallax>
+        </no-ssr>
       </div>
           <!-- Router-view -->
           <nuxt/>
